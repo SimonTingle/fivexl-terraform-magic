ba0abab824f8b2670a3e4bceb515d884cbd2ca0e
Simon Tingle <tingleteaching@gmail.com>
2025-12-14 21:02:12 +0000

    feat(final): Complete serverless static site, audit, and repository cleanup

--- coursera/terraform-labs/main.tf
+++
@@ -1,5 +0,0 @@
-resource "local_file" "games" {
-	filename = "/Users/simontingle/Desktop/WORK/PROGRAMMING/TEST_PROJECTS/coursera/terraform-labs/fav_games.txt"
-	sensitive_content  = "fifa 2021"
-	file_permission = "0755"
-}
